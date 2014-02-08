8ef4f13bc7aaa1215bd21ce706d3b940b4eeecb0
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-02-07 18:28:18 -0800

    Major re-factor using create_resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,73 +1,22 @@
 class ubuntu(
   $repos = 'main universe',
-  $release_location = 'http://archive.ubuntu.com/ubuntu/',
-  $security_location = 'http://security.ubuntu.com/ubuntu',
+  $location = 'http://archive.ubuntu.com/ubuntu/',
+  $keyserver = 'pgp.mit.edu',  
+  $sources = {},
+  $ppas = {},
   ) {
 
   Apt::Source {
-    location => $ubuntu::release_location,
-    repos    => $ubuntu::repos,
+    location => $location,
+    repos    => $repos,
   }
 
-  Class['apt'] -> Package <| provider == apt |>
+  Class[ 'apt', 'apt::source', 'apt::ppa' ] -> Package <| provider == apt |>
 
-  case $operatingsystemrelease {
-    '12.04': {
-      apt::source {
-        'precise': ;
+  create_resources('apt::source', $sources, {
+    'location' => $location,
+    'repos' => $repos,
+    'key_server' => $key_server, } )
 
-        'updates':
-          release  => 'precise-updates';
-
-        'security':
-          release  => 'precise-security';
-
-        'proposed':
-          release => 'precise-proposed',
-          pin     => 400;
-
-        'backports':
-          release => 'precise-backports',
-          pin     => 200;
-
-        'puppetlabs':
-          location   => 'http://apt.puppetlabs.com',
-          repos      => 'main dependencies',
-          key        => '4BD6EC30',
-          key_server => 'pgp.mit.edu';
-      }
-      apt::ppa {
-        'ppa:duplicity-team/ppa':
-          before => Package['duplicity'];
-
-        'ppa:git-core/ppa':
-          before => Package['git'];
-
-        'ppa:keithw/mosh': 
-          before => Package['mosh'];
-      }
-      ensure_packages([ 'duplicity', 'git', 'mosh' ])
-    }
-    '13.10': {
-      apt::source {
-        'saucy': ;
-
-        'saucy-updates':
-          release  => 'saucy-updates';
-
-        'saucy-security':
-          release  => 'saucy-security';
-
-        'puppetlabs':
-          location   => 'http://apt.puppetlabs.com',
-          repos      => 'main',
-          key        => '4BD6EC30',
-          key_server => 'pgp.mit.edu';
-      }
-    }
-    default: {
-      fail("${::lsbdistcodename} is not configured in ubuntu")
-    }
-  }
-
+  create_resources('apt::ppa', $ppas)
 }
